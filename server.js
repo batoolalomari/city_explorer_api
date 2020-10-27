edd96766eb5cf27888804a8c36bb4c5047c9c36c
batoolalomari <batoolalomari2@gmail.com>
2020-10-27 15:25:06 +0300

    weather

--- server.js
+++ server.js
@@ -11,23 +11,35 @@ app.get('/location',handelLocation);
 
 function handelLocation(req,res)
 {
-    try{
+    //try{
     let city=req.query.city;
     //let jsonData=require('./data/location.json');
-    let key='pk.74aeb760be786d944c09a41102505213';
-   superagent.get(`GET https://eu1.locationiq.com/v1/search.php?key=${key}q=${city}&format=json`).then((data)=>{
+    let key=process.env.GEOCODE_API_KEY;
+   superagent.get(`https://eu1.locationiq.com/v1/search.php?key=${key}q=${city}&format=json`).
+   then((data)=>{
+    let object=data.body[0];
+    let location=new Location(city,object.display_name,object.lat,object.lon);
 
-    res.send(data);
+    res.status(200).json(location);
+   
+    //}
+    /*catch(message){
+    res.status(500).send("Sorry, something went wrong..."); 
+    res.status(200).json(data);
+    //console.log(data);*/
 
+    }).catch(()=>{
+        res.status(500).send("Sorry, something went wrong..."); 
     });
+       
     /*let object=jsonData[0];
     let location=new Location(city,object.display_name,object.lat,object.lon);
 
     res.status(200).json(location);*/
-    }
-    catch(message){
+    //}
+    /*catch(message){
     res.status(500).send("Sorry, something went wrong..."); 
-}
+}*/
 }
 
 function Location(search_query,formatted_query,latitude,longitude)
@@ -42,15 +54,24 @@ app.get('/weather',handelWeather);
 
 function handelWeather(req,res)
 {
-    try{
-    let jsonData=require('./data/weather.json');
+   // try{
+    let city=req.query.city;
+    //let jsonData=require('./data/weather.json');
    // let object=jsonData.data;
-   let object=jsonData.map((mapData)=>{
+   let key=process.env.WEATHER_API_KEY;
+   let data=[];
+   superagent.get(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`).
+   then((Wdata)=>{
+    let object=Wdata.body.data;
+    data=object.map((Wdata)=>{
+      return  new Weather(city,object.weather.description,object.datatime);
+    });
+    res.status(200).json(data);
+}).catch(()=>{
+    res.status(500).send("Sorry, something went wrong..."); 
+})
 
-    return mapData.data;
-
-   });
-    let data=[];
+   /* 
 
     for (let index = 0; index < object.length; index++) {
          data=new Weather(object[index].description,object[index].timedate);
@@ -61,13 +82,13 @@ function handelWeather(req,res)
     }
     catch(message){
         res.status(500).send("Sorry, something went wrong..."); 
-    }
+    }*/
 }
 
-function Weather(forecast,timedate)
+function Weather(forecast,time)
 {
     this.forecast=forecast;
-    this.timedate= new Date(timedate).toDateString() ;
+    this.time= new Date(time*1000).toDateString() ;
     
 }
 
